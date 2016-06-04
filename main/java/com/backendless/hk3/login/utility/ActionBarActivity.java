df6c334ea7590454b7e31b82c08a4de17af56227
YingTong <tongyingkk@gmail.com>
2016-06-03 17:00:48 -0700

    mm

---
+++ main/java/com/backendless/hk3/login/utility/ActionBarActivity.java
@@ -0,0 +1,31 @@
+package com.backendless.hk3.login.utility;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.backendless.hk3.login.R;
+
+public class ActionBarActivity extends AppCompatActivity {
+    private ShareActionProvider mShareActionProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_bar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail_option_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.share);
+        mShareActionProvider = (ShareActionProvider)item.getActionProvider();
+        return true;
+    }
+}
