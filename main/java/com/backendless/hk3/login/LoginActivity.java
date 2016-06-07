e242534971d74a2f8d560f2e7d9ebb62a88428b9
YingTong <tongyingkk@gmail.com>
2016-06-07 14:07:35 -0700

    ..

--- main/java/com/backendless/hk3/login/LoginActivity.java
+++ main/java/com/backendless/hk3/login/LoginActivity.java
@@ -7,8 +7,6 @@ import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
